2d24ede4a6e2ba637b64549d70e8a12a933fe387
Vsakpc <pcakvs8820@gmail.com>
2024-04-24 16:55:57 +0530

    hello-world

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello,suresh,How are you?"
+echo "suresh:: Hi Ramesh, i am fine and How are you?"
+echo "Ramesh:: I am fine too, how is your work?"
+echo "suresh:: not bad, I am thinking to upgrade to Devops?"
