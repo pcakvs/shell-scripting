0eca53def813f1b4dd261664a34e494f41229b6d
Vsakpc <pcakvs8820@gmail.com>
2024-04-24 18:45:09 +0530

    hello-world

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+USERID =$(id -u)
+
+if [ $USERID -ne 0]
+then
+ echo "please run this script with root access"
+ else
+ echo "your are the super user"
+ fi
 dnf install mysql -y
+
+echo "is script proceeding"
