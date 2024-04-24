c1be9b643fe7c2b193923535de8105f4c0ad6320
Vsakpc <pcakvs8820@gmail.com>
2024-04-24 17:41:10 +0530

    hello-world

---
+++ 07-Array.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "MURUARI")
+
+#size of above array is 3
+
+#index are 0,1,2
+#list always starts with 0
+
+echo "first movie is: ${MOVIES[0]}"
+echo "second movie is : ${MOVES[1]}"
+echo "all movies list is : ${MOVIES[@]}"
