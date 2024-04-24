d332739992e6a4293b90149dddedf0b315eba632
Vsakpc <pcakvs8820@gmail.com>
2024-04-24 17:24:34 +0530

    hello-world

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username:"
+
+read USERNAME # here USEARNAME is a variable
+
+echo "please enter password:"
+
+read PASSWORD
+
+echo "username is : $USERNAME , password is : $PASSWORD"
